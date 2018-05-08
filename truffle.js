77d418ff2c1308d9c500f302b557ff133d3e8797
Yuming He <yuming.p.he@gmail.com>
2018-05-07 23:01:49 -0400

    Setup

--- truffle.js
+++ truffle.js
@@ -1,4 +1,40 @@
+require('babel-register');
+require('babel-polyfill');
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "crawl fade put couch jewel wine basket million license indoor push sniff";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gasPrice: 0,
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/KetwbGBGwNagOnjgPUkN")
+      },
+      gas: 4600000,
+      network_id: 3
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/KetwbGBGwNagOnjgPUkN")
+      },
+      gas: 4600000,
+      network_id: 4
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/KetwbGBGwNagOnjgPUkN")
+      },
+      gas: 4712388,
+      gasPrice: 100000000000,
+      network_id: 1
+    },
+  },
 };
